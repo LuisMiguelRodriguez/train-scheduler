b1ae2799b0ff949a4b89316c3c27fda0ca5534f7
LuisMiguelRodriguez <luis.the.coder@gmail.com>
2017-07-25 16:00:06 -0700

    Fixed remaining time issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,20 +9,24 @@ $(document).ready(function (){
       storageBucket: "tanukitektube.appspot.com",
       messagingSenderId: "537477124984"
     };
+
     firebase.initializeApp(config);
 
+    //Reference to root of database
     var database = firebase.database();
-    var ref = database.ref('scores');
 
+    // On click event for submiting a train schedule
     $("#addTrain").on("click", function(event) {
-    	event.preventDefault(); //no button reset
 
-    	//set user input values to variables
+    	event.preventDefault();
+
+    	//Grabbing data from form
     	var trainName = $("#train-name").val().trim(),
     	    destination = $("#destination").val().trim(),
           firsTrainD = $('#first-train').val().trim(),
     	    frequency = $("#frequency").val().trim();
 
+      // Creating an object with form data to be push to database
     	var newTrain = {
     		train: trainName,
     		trainGoing: destination,
@@ -30,6 +34,7 @@ $(document).ready(function (){
         firstTrain: firsTrainD
     	};
 
+      //Pushing data to the database
     	database.ref().push(newTrain);
 
     	//clears elements before adding new text
@@ -43,14 +48,18 @@ $(document).ready(function (){
 
     //################################################################
     //################################################################
-
+      //does an initial loop on first load of all the data
+      //and adds new new data as added
       database.ref().on("child_added", function(data, prevChildKey) {
 
+      //Grabbing values from database for current child
   		var trainName = data.val().train,
   		  destination =data.val().trainGoing,
   		  firstTime = data.val().trainComing,
   		  frequency = data.val().frequency,
         firstTrainDep = data.val().firstTrain,
+
+        // Converting time values to usable form to manipulate
         m = moment(firstTrainDep, "hmm").format("HH:mm"),
         stringed = m.split(':').join(''),
         ifirstTrain = parseInt(stringed),
@@ -60,20 +69,29 @@ $(document).ready(function (){
         iCurrentTime = parseInt(cTimeStringed),
         iFreq = parseInt(frequency);
 
+      //Loop starts with firsttrain time and adds frequency of train
+      //until it just passes the current time which will give the next
+      //train time
+
       while(iCurrentTime > ifirstTrain){
         ifirstTrain += iFreq;
       }
 
+      //Test after loop
       console.log(ifirstTrain);
 
-      var remainingTime = ifirstTrain - iCurrentTime,
-  		  nextArrival = ifirstTrain;
+      //Subtracts next train Time from loop to current time
+      //to come up remainingtime
+      var remainingTime = ifirstTrain - iCurrentTime;
+        nextTrain = moment(ifirstTrain,  "hmm").format("hh:mm");
 
+      //Appends table data
   		$("#train-schedule").append(
         "<tr><td>" + trainName +
         "</td><td>" + destination +
         "</td><td>" + frequency +
-        "</td><td>" + ifirstTrain +
+        // added remaining time to current time to get next train Arrival
+        "</td><td>" + moment().add(remainingTime,'minutes').format("hh:mm") +
         "</td><td>" + remainingTime +
         "</td><tr>"
       );
@@ -82,11 +100,7 @@ $(document).ready(function (){
 
 });
 
-
-
-
-
-
+  // testing data 
   // ref.on('value', gotData, errData);
   //
   // function gotData(data) {
